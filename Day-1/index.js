27d089022f505c8d971c8522506649f702b239cc
Sushilverma002 <sushilvarma9911@gmail.com>
2025-01-02 23:42:58 +0530

    day1

---
+++ Day-1/index.js
@@ -0,0 +1,35 @@
+console.log("this is external file");
+
+// -> comment in js
+/* 
+sdfds
+sdfd
+sdfdf*/
+
+// data types  : what is the type of are variable .
+
+// 1. Number/INT
+var length = 5;
+
+// 2. Strings -> ""
+
+var name = "NEHA";
+
+// 3. Booleans :
+
+var x = true;
+var y = false;
+
+// 4. Object : -> key : value
+
+package_val = {
+  start: "npm vite",
+};
+
+// 5. array : similar type of data ko store karta hai.... let say numeric, string.
+
+const cars = ["nano", "maruti", "xyzzz"];
+
+// 6. date object:
+const date = new Date("2025-01-02");
+console.log(date);
