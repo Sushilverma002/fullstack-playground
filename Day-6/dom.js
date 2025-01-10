601f22d4d2a7a815c8c5fb0a3031b752b46dc91b
Sushilverma002 <sushilvarma9911@gmail.com>
2025-01-10 22:19:58 +0530

    day 6

---
+++ Day-6/dom.js
@@ -0,0 +1,24 @@
+/*DOM : Document object model 
+* when ever a webpage is loaded the browser will create a DOM 
+TREE LIKE Structure.
+
+                        document
+                            |
+                            root element <html>
+            head              body               script
+            meta                a                   data type
+            title             li, ol,image          id call kar action perform 
+
+object model -> javascript power with the help of this dynamic HTML.
+change all the HTML element, attributes in the page. css style , exisiting HMTL element , event perform ,  new HTML events can be created.
+
+W3C (WORLD WIDE WEB) STANDARD :3 DOM
+
+1. core DOM: standard model for all type of documents
+2. xml DOM: standard model for xml documents
+3. HTML DOM: Standard model for the HTML documents
+
+HTML DOM: can be accessed with Javascript and even other programing languages.
+
+
+*/
