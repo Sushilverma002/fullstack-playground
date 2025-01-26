e5857bd56c7edf9691bbea4476dc9bdd65366365
Sushilverma002 <sushilvarma9911@gmail.com>
2025-01-26 22:05:28 +0530

    26th jan 2025

---
+++ Day-13/index.js
@@ -0,0 +1,33 @@
+// js objects
+
+const person = new Object();
+
+person.first = "A";
+person.second = "B";
+
+console.log(person);
+
+// functions 1 : assign(), entries()
+
+// Target
+const person1 = {
+  firstName: "rahul",
+  secondName: "kumar",
+  age: 15,
+};
+
+// source
+const person2 = {
+  firstName: "ram",
+  secondName: "sharma",
+};
+
+// source-> target
+Object.assign(person1, person2);
+console.log(person1);
+
+// entires
+let text = Object.entries(person1);
+console.log(text);
+
+// function : call by value and call by reference
