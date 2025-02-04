709c562f1c7c8b6c643afa1dd095692049777972
Sushilverma002 <sushilvarma9911@gmail.com>
2025-02-04 22:15:43 +0530

    changes

--- Weather/script.js
+++ Weather/script.js
@@ -1,8 +1,21 @@
-/*API key :- 684a23639dbc72d1822a22ef9ae5193a*/
+let searchInput = document.querySelector(".searchbar");
+let serachButton = document.querySelector(".search-button");
 
-const APIKEY = "684a23639dbc72d1822a22ef9ae5193a";
-const lat = 51.5074; 
-const lon = -0.1278;
-const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}&units=metric&lang=en`
-let SEARCHBAR = document.querySelector(".searchbar");
-let SEARCHICON = document.querySelector("#search-icon");
+const apiKey = "684a23639dbc72d1822a22ef9ae5193a";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+
+async function checkWheather(city) {
+  const response = await fetch(
+    apiUrl + city + `&appid=${apiKey}` + "&units=mertic"
+  );
+  console.log(response);
+  var data = await response.json();
+  console.log(data);
+  //   https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=684a23639dbc72d1822a22ef9ae5193a&units =mertic
+  document.getElementById("temp").innerHTML = Math.round(data.main.temp) + "'C";
+  document.querySelectorinnerHTML = Math.round(data.main.temp) + "'C";
+}
+
+serachButton.addEventListener("click", () => {
+  checkWheather(searchInput.value);
+});
