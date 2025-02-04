5e45e862c1017e045683277cf948e5cd57c4992b
nehachaudhary972 <neha972.chaudhary@gmail.com>
2025-02-04 19:09:27 +0530

    changes

--- Weather/script.js
+++ Weather/script.js
@@ -1,5 +1,8 @@
 /*API key :- 684a23639dbc72d1822a22ef9ae5193a*/
 
-
-let SEARCHBAR = document.querySelector("searchbar")
-let SEARCHICON = document.querySelector("search-icon")
+const APIKEY = "684a23639dbc72d1822a22ef9ae5193a";
+const lat = 51.5074; 
+const lon = -0.1278;
+const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={APIkey}&units=metric&lang=en`
+let SEARCHBAR = document.querySelector(".searchbar");
+let SEARCHICON = document.querySelector("#search-icon");
