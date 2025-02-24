fcf34928aba176368641ab0903483a248b10333a
Sushilverma002 <sushilvarma9911@gmail.com>
2025-02-24 21:59:59 +0530

    plan with starter

--- plan-with-love-starter/src/App.js
+++ plan-with-love-starter/src/App.js
@@ -6,10 +6,24 @@ import Tours from "./component/Tours";
 const App = () => {
   const [tours, setTours] = useState(data);
 
+  function tourDelete(id) {
+    const newTour = tours.filter((tour) => tour.id !== id);
+    setTours(newTour);
+  }
+
+  if (tours.length === 0) {
+    return (
+      <div>
+        <h1>No tour left. Please refresh</h1>
+        <button className="whit-btn" onClick={() => setTours(data)}>
+          Refresh
+        </button>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1>Plan with Love</h1>
-      <Tours tours={tours} />
+    <div className="App">
+      <Tours tours={tours} tourDel={tourDelete} />
     </div>
   );
 };
