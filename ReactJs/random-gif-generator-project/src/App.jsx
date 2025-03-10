47e9ca3ebd454454b29110eecbac685c05e698d4
Sushilverma002 <sushilvarma9911@gmail.com>
2025-03-10 22:25:56 +0530

    changes main

--- ReactJs/random-gif-generator-project/src/App.jsx
+++ ReactJs/random-gif-generator-project/src/App.jsx
@@ -1,12 +1,19 @@
 import Random from "./components/Random";
+import Tag from "./components/Tag";
 
 export default function App() {
   return (
-    <div className="w-auto h-[1024px] flex flex-col background">
-      <h1 className="text-4xl font-bold px-4 py-4 mt-6 mr-12 ml-12 rounded-2xl   flex justify-center items-center bg-white border-none">
-        RANDOM GIFs
+    <div className="w-full h-screen flex flex-col background relative overflow-x-hidden items-center">
+      <h1
+        className=" bg-white rounded-lg w-11/12 text-center mt-[40px]
+     px-10 py-2 text-4xl font-bold "
+      >
+        RANDOM GIFS
       </h1>
-      <Random />
+      <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        <Random />
+        <Tag />
+      </div>
     </div>
   );
 }
--- ReactJs/random-gif-generator-project/src/components/Random.jsx
+++ ReactJs/random-gif-generator-project/src/components/Random.jsx
@@ -1,52 +1,60 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
-const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+import Sipnner from "./Sipnner";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+import useGif from "../hooks/useGif";
+// const GIPHY_API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 // https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}
 export default function Random() {
-  const url = `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}&`;
-  console.log(url);
-  const [gif, setGif] = useState("");
+  // const url = `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}`;
+  // console.log(url);
+  // const [gif, setGif] = useState("");
+  // const [Loading, setLoading] = useState(false);
 
-  const generateGif = async () => {
-    const response = await axios.get(url);
-    const gifUrl = response.data.data.user.avatar_url;
-    // console.log(response);
-    setGif(gifUrl);
-  };
+  // const generateGif = async () => {
+  //   setLoading(true);
+  //   const response = await axios.get(url);
+  //   const gifUrl = response.data.data.images.downsized_large.url;
 
-  useEffect(() => {
-    generateGif();
-  }, []);
+  //   setGif(gifUrl);
+  //   setLoading(false);
+  // };
 
-  console.log(gif);
-  function handleGenerateGifs() {
-    generateGif();
-  }
+  // useEffect(() => {
+  //   generateGif();
+  // }, []);
+
+  // console.log(gif);
+  // function handleGenerateGifs() {
+  //   generateGif();
+  // }
+  const { gif, Loading, generateGif } = useGif();
   return (
-    <div>
-      <div>
-        <h2>A RANDOM GIF</h2>
-        {/* <img
+    <div
+      className="w-1/2  bg-yellow-400 rounded-lg border border-black
+    flex flex-col items-center gap-y-5 mt-[15px]"
+    >
+      {" "}
+      <h1 className="mt-[15px] text-2xl underline uppercase font-bold">
+        {" "}
+        A Random Gif
+      </h1>
+      {/* <img
           src={gif}
           alt="Random GIF"
           className="max-w-[30rem] px-4 object-contain"
         /> */}
-        <img src={gif} />
-        <button onClick={handleGenerateGifs}>Generate</button>
-      </div>
-      <div>
-        <h2>A RANDOM GIF</h2>
-        {/* <img
-          src={gif}
-          alt="Random GIF"
-          className="max-w-[30rem] px-4 object-contain"
-        /> */}
-        <img src={gif} />
-        <input></input>
-        <button onClick={handleGenerateGifs}>Generate</button>
-      </div>
+      {Loading ? (
+        <Sipnner />
+      ) : (
+        <img src={gif} className="max-w-[30rem] px-4 object-contain" />
+      )}
+      <button
+        onClick={generateGif}
+        className="w-10/12 bg-yellow-500 text-lg py-2 rounded-lg mb-[20px]"
+      >
+        Generate
+      </button>
     </div>
   );
 }
