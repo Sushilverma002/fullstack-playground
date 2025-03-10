e18c56a7af5f7da4150af86c9839fde2e1a82878
Sushilverma002 <sushilvarma9911@gmail.com>
2025-03-10 22:45:30 +0530

    changes main

---
+++ ReactJs/blogs-context-project/src/App.js
@@ -0,0 +1,13 @@
+import { Blog } from "./components/Blog";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+
+export default function App() {
+  return (
+    <div>
+      <Blog />
+      <Footer />
+      <Header />
+    </div>
+  );
+}
